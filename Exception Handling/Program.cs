0b38619b118051bcf41e5f46641393c05ee68aa9
Anthony Smith <mrpresidentsmith@gmail.com>
2019-05-01 12:20:55 -0700

    Assignment completed.

---
+++ Exception Handling/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exception_Handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int intvalue = 0;
+
+            try
+            {
+                Console.Write("Enter an integer:  ");
+                intvalue = int.Parse(Console.ReadLine());
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine("That is NOT even a number...");
+            }
+            catch (OverflowException oe)
+            {
+                Console.WriteLine("That is NOT an integer...");
+            }
+
+            finally
+            {
+                Console.WriteLine("Nice work.  This program is done.");
+            }
+
+
+        }
+    }
+}
